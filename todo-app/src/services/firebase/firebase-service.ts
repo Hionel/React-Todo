7afc6c6116e896a70cdddb684f0833cac3fa4fe5
Hionel <104563991+Hionel@users.noreply.github.com>
2024-07-07 02:22:15 +0300

    re-done the environment configuration and gitignore update

--- todo-app/src/services/firebase/firebase-service.ts
+++ todo-app/src/services/firebase/firebase-service.ts
@@ -2,20 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyA1CNXtc3DLRwXVf5lCB4fVILzCKJQ0ZhQ",
-	authDomain: "react-todo-750af.firebaseapp.com",
-	projectId: "react-todo-750af",
-	storageBucket: "react-todo-750af.appspot.com",
-	messagingSenderId: "52528618441",
-	appId: "1:52528618441:web:e6a79775f16b50b3236618",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseFirestore = getFirestore(firebaseApp);
+
+export default firebaseApp;
